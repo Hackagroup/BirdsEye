d99c2bdc4b8b032aa6df15f935daf0b76445d1b2
amanuel2 <amanuelb43@gmail.com>
2020-11-20 13:30:35 -0500

    Search backend migrated

--- client/src/views/dashboard/index.js
+++ client/src/views/dashboard/index.js
@@ -1,26 +1,9 @@
 import React, { useState } from 'react'
 import { Helmet } from 'react-helmet'
-import API from '../../api'
-
 function Dashboard() {
-  const [search, setSearch] = useState('')
-  const [loading, setLoading] = useState(false)
-  const [tweets, setTweets] = useState([])
-
-  async function handleSubmit() {
-    setLoading(true)
-    const response = await API.search.get('', { searchQuery: search })
-    if (response.message == null) {
-      const { tweets } = response
-      const { statuses } = tweets
-      console.log(tweets)
-      setTweets(statuses ?? [])
-    }
-    setLoading(false)
-  }
 
   async function createTweet() {
-    
+    // create tweet function
   }
 
   return (
@@ -34,32 +17,6 @@ function Dashboard() {
          <input type="text" placeholder="Type a tweet" onclick={createTweet}/>
         </div>
       <br/>
-      {loading ? (
-        <div>Fetching tweets...</div>
-      ) : (
-        <>
-          <div>Search some tweets:</div>
-          <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
-          <button type="button" onClick={handleSubmit}>
-            Go
-          </button>
-          <hr />
-          <div>Search result</div>
-          {tweets.map((tweet) => {
-            const hashtags = tweet?.entities?.hashtags ?? []
-            return (
-              <div key={tweet.id_str}>
-                <div>Text: {tweet.text}</div>
-                <div>Created at: {tweet.created_at}</div>
-                <div>
-                  Hashtags: {hashtags.length > 0 ? hashtags.map((x) => x.text).join(', ') : 'None'}
-                </div>
-                <hr />
-              </div>
-            )
-          })}
-        </>
-      )}
     </>
   )
 }
