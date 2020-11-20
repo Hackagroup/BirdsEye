9bed2651c36bce45baebdb3daedf21fc1b052e9c
amanuel2 <amanuelb43@gmail.com>
2020-11-20 13:07:10 -0500

    SPA Links

--- client/src/views/navigation/index.js
+++ client/src/views/navigation/index.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react'
 import { Helmet } from 'react-helmet'
+import { Link } from 'react-router-dom'
 import API from '../../api'
 
 
 function Navigation(){
     return(
         <nav>
-          <a href="#">Home</a> |
-          <a href="#">Search</a> |
-          <a href="#">Settings</a> |
-          <a href="#">Logout</a>
+          <Link to="/">Home</Link>|
+          <Link to="/search">Search</Link>|
+          <Link to="/settings">Settings</Link>|
+          <Link to="/logout">Logout</Link>  
         </nav>
     )
 }
