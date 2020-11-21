b90416b8f48e97d82c794726c084638de051ff17
Ajay Dabas <se.dabasajay@gmail.com>
2020-11-21 18:09:06 +0530

    implement our own twitter login button

--- client/src/views/landing/index.js
+++ client/src/views/landing/index.js
@@ -1,32 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Redirect } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { Helmet } from 'react-helmet'
 import { API_ENDPOINTS } from '../../constants'
 import { SET_USER } from '../../actions/types'
-import TwitterLogin from 'react-twitter-auth/lib/react-twitter-auth-component.js'
-import TwitterLogin2 from 'react-twitter-login'
+import TwitterLoginButton from '../../components/TwitterLoginButton'
 import './landing.css'
 import logo from '../../assets/logo-white.png'
-import { Button } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles';
-import SaveIcon from '@material-ui/icons/Save';
 
 
 function Login() {
+  const [loading, setLoading] = useState(false)
+  const [errMessage, setErrMessage] = useState('')
   const user = useSelector((state) => state.user)
   const dispatch = useDispatch()
   const { isAuthenticated } = user
-  const useStyles = makeStyles((theme) => ({
-    button: {
-      margin: theme.spacing(1),
-    },
-  }));
-
-  const classes = useStyles();
 
   function handleFailure(err) {
     console.log(err)
+    setLoading(false)
+    setErrMessage('Error! Try again.')
   }
 
   function handleSuccess(res) {
@@ -39,18 +32,13 @@ function Login() {
           userCredentials,
         },
       })
+      setLoading(false)
     })
   }
 
-  if (isAuthenticated) {
+  if (!loading && isAuthenticated) {
     return <Redirect to="/dashboard" />
   }
-
-  const authHandler = (err, data) => {
-    console.log(err)
-    console.log(data)
-  }
-
 
   return (
     <>
@@ -67,27 +55,18 @@ function Login() {
           <h3>"A closer look at the big picture"</h3>
           <br></br>
           <br></br>
-          <TwitterLogin
-              loginUrl={API_ENDPOINTS.LOGIN}
-              onFailure={handleFailure}
-              onSuccess={handleSuccess}
-              requestTokenUrl={API_ENDPOINTS.REQUEST_TOKEN_URL}
-              children={
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    size="large"
-                    className={classes.button}
-                    startIcon={<SaveIcon />}
-                  ></Button>
-              }
-            >
-            </TwitterLogin>
-          {/* <TwitterLogin2
-            authCallback={authHandler}
-            consumerKey={process.env.TWITTER_CONSUMER_API_KEY}
-            consumerSecret={process.env.TWITTER_CONSUMER_API_SECRET}
-          /> */}
+          <TwitterLoginButton
+            loginUrl={API_ENDPOINTS.LOGIN}
+            onFailure={handleFailure}
+            onSuccess={handleSuccess}
+            requestTokenUrl={API_ENDPOINTS.REQUEST_TOKEN_URL}
+            text={errMessage === '' ? (loading ? 'Please wait' : 'Authorize') : errMessage}
+            disabled={loading}
+            onClickCallback={() => {
+              setLoading(true)
+              setErrMessage('')
+            }}
+          />
         </div>
       </section>
     </>
