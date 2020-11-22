2617864086566bc256a797c476ab1ca3599b8b8c
Ajay Dabas <se.dabasajay@gmail.com>
2020-11-22 18:05:30 +0530

    reverting some changes and deleting search page

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,17 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Switch, Route, useLocation } from 'react-router-dom'
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
-
-import { AnimatePresence } from 'framer-motion'
 
 import PrivateRoute from './components/PrivateRoute'
 import Navbar from './components/Navbar'
 
 import Landing from './views/landing/'
 import Dashboard from './views/dashboard/'
-import Search from './views/search'
 import Settings from './views/settings'
 
 import store from './store'
 import { SET_USER } from './actions/types'
 import isEmpty from './utils/isEmpty'
-
-import { createBrowserHistory } from 'history'
-const history = createBrowserHistory()
-
-function Wrapper() {
-  return (
-    <Provider store={store}>
-      <Router>
-        <Route component={App} />
-      </Router>
-    </Provider>
-  )
-}
 
 // Check for user credentials in local storage (in-case app is reloaded/reopened)
 if (!isEmpty(localStorage.userCredentials)) {
@@ -41,20 +25,20 @@ if (!isEmpty(localStorage.userCredentials)) {
 }
 
 function App() {
-  const location = useLocation()
   return (
-    <>
-      <Navbar />
-        <Switch location={location} key={location.pathname}>
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Switch>
           <Route exact path="/landing" component={Landing} />
           <PrivateRoute exact path="/dashboard" component={Dashboard} />
-          <PrivateRoute exact path="/search" component={Search} />
           <PrivateRoute exact path="/settings" component={Settings} />
           {/* Default route */}
           <Route component={Landing} />
         </Switch>
-    </>
+      </Router>
+    </Provider>
   )
 }
 
-export default Wrapper
+export default App
