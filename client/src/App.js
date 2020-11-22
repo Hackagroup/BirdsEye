3d4e39522b192ecbd96093926a8c941c363f562e
amanuel2 <amanuelb43@gmail.com>
2020-11-22 01:55:17 -0500

    Small changes

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,16 @@ import store from './store'
 import { SET_USER } from './actions/types'
 import isEmpty from './utils/isEmpty'
 
+
+import SlideRouter, { initSlideRouter } from 'react-slide-animation-router'
+import { createBrowserHistory } from 'history'
+const history = createBrowserHistory()
+
+initSlideRouter({
+  history,
+  routeAnimationDuration: 350
+})
+
 // Check for user credentials in local storage (in-case app is reloaded/reopened)
 if (!isEmpty(localStorage.userCredentials)) {
   store.dispatch({
@@ -31,12 +41,14 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route exact path="/landing" component={Landing} />
-          <PrivateRoute exact path="/dashboard" component={Dashboard} />
-          <PrivateRoute exact path="/search" component={Search} />
-          <PrivateRoute exact path="/settings" component={Settings} />
-          {/* Default route */}
-          <Route component={Landing} />
+          
+            <Route exact path="/landing" component={Landing} />
+            <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            <PrivateRoute exact path="/search" component={Search} />
+            <PrivateRoute exact path="/settings" component={Settings} />
+            {/* Default route */}
+            <Route component={Landing} />
+          
         </Switch>
       </Router>
     </Provider>
